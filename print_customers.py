92149e588c4a9e2ac82681c247ebc291609b8eb8
kw@test.com <kw2test.com>
2022-09-01 13:09:29 -0500

    changes

---
+++ print_customers.py
@@ -0,0 +1,28 @@
+#this will print customers and will only advance one you hit the enter key.
+# 
+#  
+#import csv
+import csv
+
+#open file
+infile = open('customers.csv', 'r')
+
+#read contents into csv object
+csvfile = csv.reader(infile, delimiter = ',')
+ 
+#skip the first row
+
+next(csvfile)
+
+#build for loop 
+for record in csvfile: 
+    print('ID:', record[0]) 
+    print('First Name:', record[1])
+    print('Last Name:', record[2])
+    print('City:', record[3]) 
+    print('Country:', record [4])
+    print('Phone', record[5])
+    
+    #establish input
+
+    input()
