a2fe66e8516a3e18ad539936fa0f0d63a7f007d0
kw@test.com <kw2test.com>
2022-09-05 16:05:26 -0500

    new changes

---
+++ employee_bonus.py
@@ -0,0 +1,32 @@
+#This program will read the employee pay.csv file and print the details of each employee. 
+#Details must include bonus pay (which has to be calculated). The program will pause after each employee. 
+
+#import csv module 
+import csv
+
+#define main 
+def main():
+    
+    #open file
+    infile = open('EmployeePay.csv', 'r')
+
+    #read contents into a csv object
+    csvfile = csv.reader(infile, delimiter = ',')
+
+    #skip the first row
+    next(csvfile)
+
+    #build for loop 
+    for line in csvfile: 
+        print('ID:', line [0])
+        print('Full Name:', line[1] + ' ' + line [2])
+        print('Salary:', line[3])
+        print('Bonus:', line [4])
+        totalpay = (float(line[3]) * float(line [4])) + float(line[3])
+        print('Total Pay:', totalpay)
+
+        #establish pause
+        input()
+
+#call main 
+main()
